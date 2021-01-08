129b4b73df7ef928e4b61af7e83f28998d5a283d
Caroline Araujo <ca.araujosouza@gmail.com>
2021-01-08 00:28:18 -0300

    add parallelStream

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 class Curso {
 	private String nome;
@@ -33,8 +37,24 @@ public class ExemploCursos {
 		
 		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
 		
-		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+		OptionalDouble media = cursos.stream()
+				.filter(c -> c.getAlunos() >= 100)
+				.mapToInt(Curso::getAlunos)
+				.average();
 		
-		System.out.println(sum);
+		//System.out.println(sum);
+		
+//	cursos.stream()
+//		.filter(c -> c.getAlunos() >= 100)
+//		.findAny()
+//		.ifPresent(c -> System.out.println(c.getNome()));
+	
+		cursos.parallelStream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toMap(
+					c -> c.getNome(),
+					c -> c.getAlunos()))
+			.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
+		
 	}
 }
