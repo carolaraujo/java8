adf4fbe70c16d6e612e60227e0ff1b3dcde247f8
Caroline Araujo <ca.araujosouza@gmail.com>
2021-01-07 23:22:37 -0300

    add method references

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -3,6 +3,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+
+import jdk.internal.misc.FileSystemOption;
 
 public class OrdenaString {
 
@@ -22,15 +25,24 @@ public class OrdenaString {
 				return 0;			
 		});
 		
-		palavras.sort((s1, s2)-> Integer.compare(s1.length(), s2.length()));
+		//palavras.sort((s1, s2)-> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+		Comparator<String> comparator = Comparator.comparing(s -> s.length());
+		palavras.sort(comparator);
 		
 		System.out.println(palavras);
 		
-		Consumer<String> impressor = s -> System.out.println(s);
+		Consumer<String> impressor = System.out::println;
 		palavras.forEach(impressor);
 		
 		
-		palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
 	}
 }
 
